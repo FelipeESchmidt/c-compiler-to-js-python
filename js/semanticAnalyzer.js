0e00cab0710419e01a603ec3081a3dddb11af5a5
Felipe E. Schmidt <schmidtfe2@gmail.com>
2023-06-10 15:35:41 -0300

    feat: creating function to process body inside code blocks and validating a with some semantic logic, creating functions to verify code structures and functions to create their structure

--- js/semanticAnalyzer.js
+++ js/semanticAnalyzer.js
@@ -23,6 +23,23 @@ const processor = (ast) => {
   // Aqui buscamos as funções do código e organizamos melhor as informações da função
   const codeFunctions = findFuncs(ast.body);
 
+  // Após buscar as funções processamos o body e arrumamos os argumentos
+  const codeFunctionsProcessed = codeFunctions.map((codeFunction) => ({
+    ...codeFunction,
+    body: processBody(codeFunction.body),
+    args: updateFunctionArguments(codeFunction.args),
+  }));
+
+  const functionPack = {
+    type: "Functions",
+    body: codeFunctionsProcessed,
+  };
+  // At the end, we'll define our final abstract syntax tree structure and name it TheBigAST:)
+  // and push our 2 top level arrays: globalItems and functionPack into it.
+  // TheBigAST will then be ready to be compiled
+  const TheBigAST = [globalItem, functionPack];
+
+  return TheBigAST;
 };
 
 /**
@@ -223,4 +240,190 @@ const findFuncs = (astBody) => {
 
   // Retorna as funções encontradas e organizadas
   return functions;
+};
+
+// Esta função processa os cases de um switch
+const processSwitch = (inside, current) => {
+  let count = 0;
+  const cases = [];
+  const args = inside[current].arguments;
+  const codeCave = inside[current - 1];
+  args.reverse();
+  let reverseCaseParts = [];
+  while (count < args.length) {
+    if (args[count].type !== "Colon") {
+      reverseCaseParts.push(args[count]);
+    } else {
+      const currentCaseType = args[count + 1].type;
+      const currentCaseValue = args[count + 1].value;
+      const currentStatementsGroup = reverseCaseParts.reverse();
+      if (args[count + 1].value === "default") {
+        count++;
+      } else if (args[count + 2].value === "case") {
+        count += 2;
+      }
+      reverseCaseParts = [];
+      const caseStatements = processBody(currentStatementsGroup);
+      cases.unshift({
+        caseType: currentCaseType,
+        caseValue: currentCaseValue,
+        caseStatements: caseStatements,
+      });
+    }
+    count++;
+  }
+
+  return switchCallBuilder(codeCave, cases);
+};
+
+// Esta função processa o body montando e validando as sequencias
+const processBody = (inside) => {
+  const statements = [];
+
+  let current = 0;
+
+  let start = 0;
+
+  const pushStatement = (statement, increaseCurrent = true) => {
+    statements.push(statement);
+    if (increaseCurrent) current++;
+  };
+
+  while (current < inside.length) {
+    const part = inside[current];
+
+    if (isFunctionCallSequence(part, inside, current)) {
+      pushStatement(functionCallBuilder(part));
+      continue;
+    }
+
+    if (isCodeStructureSequence(part, inside, current)) {
+      const codeCave = inside[current - 1];
+      const codeStructureType = inside[current - 2].value.toLowerCase();
+
+      switch (codeStructureType) {
+        case "if":
+          {
+            const isElseIf = identifyElseIf(inside[current - 3]);
+            pushStatement(
+              ifCallBuilder(codeCave, processBody(part.arguments), isElseIf)
+            );
+          }
+          break;
+
+        case "while":
+          pushStatement(
+            whileCallBuilder(codeCave, processBody(part.arguments))
+          );
+          break;
+
+        case "for":
+          pushStatement(forCallBuilder(codeCave, processBody(part.arguments)));
+          break;
+
+        case "switch":
+          pushStatement(processSwitch(inside, current));
+          break;
+
+        default:
+          throw new TypeError("Sintaxe inválida!");
+      }
+
+      continue;
+    }
+
+    if (isSmallCodeStructureSequence(part, inside, current)) {
+      const codeStructureType = inside[current - 1].value.toLowerCase();
+
+      if (codeStructureType === "else") {
+        pushStatement(elseCallBuilder(processBody(part.arguments)));
+        continue;
+      }
+
+      if (isDoCodeStructureSequence(part, inside, current)) {
+        const codeCave = inside[current + 2];
+        pushStatement(doCallBuilder(codeCave, processBody(part.arguments)));
+        current += 2; // Pula até o while
+        continue;
+      }
+    }
+
+    if (isStructCodeStructureSequence(part, inside, current)) {
+      const prevElement = inside[current - 1];
+      const doublePrevElement = inside[current - 2];
+      if (doublePrevElement.value === "struct") {
+        pushStatement(
+          structCallBuilder(prevElement.value, processBody(part.arguments))
+        );
+        continue;
+      }
+      throw new TypeError("Sintaxe inválida!");
+    }
+
+    if (part.type === "Terminator") {
+      const phrase = [];
+
+      while (start <= current) {
+        const currentInsideStart = inside[start];
+        if (
+          currentInsideStart.type === "Word" &&
+          terminatorStepper[currentInsideStart.value]
+        ) {
+          start += terminatorStepper[currentInsideStart.value];
+          continue;
+        }
+
+        phrase.push({
+          type: currentInsideStart.type,
+          value: currentInsideStart.value,
+        });
+        start++;
+      }
+
+      pushStatement(statementBuilder(phrase), false);
+    }
+
+    const lastItem = current === inside.length - 1;
+
+    if (lastItem && !endableTypes.includes(part.type)) {
+      throw new TypeError("Seu código possui um erro em algum lugar!");
+    }
+
+    current++;
+    continue;
+  }
+
+  return statements;
+};
+
+/**
+ * Função ajusta os argumentos de uma função
+ * @param {any[]} cave
+ * @returns
+ */
+const updateFunctionArguments = (cave) => {
+  if (!cave.length) return [];
+
+  const reducedArgs = cave.reduce(
+    (prev, current) => {
+      if (current.type === "Delimiter") return [...prev, []];
+
+      prev[prev.length - 1].push(current);
+      return prev;
+    },
+    [[]]
+  );
+
+  const params = reducedArgs.map((reducedArg) => {
+    const argumentType = reducedArg.shift();
+
+    const argumentName = reducedArg.find((arg) => arg.type === "Word");
+
+    return {
+      type: argumentType.value,
+      name: argumentName.value,
+    };
+  });
+
+  return params;
 };
---
+++ js/semanticSequencesBuilder.js
@@ -0,0 +1,262 @@
+const semanticSequences = {
+  functionCall: "WordCodeCaveTerminator",
+  codeStructure: "WordCodeCaveCodeDomain",
+  smallCodeStructure: "WordCodeDomain",
+  doCodeStructure: "WordCodeDomainWordCodeCave",
+  structCodeStructure: "WordWordCodeDomainTerminator",
+};
+
+const terminatorStepper = {
+  if: 3,
+  for: 3,
+  switch: 3,
+  while: 3,
+  do: 5,
+  else: 2,
+};
+
+const endableTypes = ["Terminator", "CodeDomain"];
+
+/**
+ * Agrupa elementos pelo tipo
+ * @param {{type: string}[]} elements
+ * @returns {string} elementos agrupados pelo tipo
+ */
+const agroupElemetsType = (...elements) =>
+  elements.map((el) => el.type).join("");
+
+/**
+ * Verifica se é chamada de uma função                    [Word]  [CodeCave]    [Terminator]
+ * A chamada de função precisa ter a seguinte sequencia   [nome]  [argumentos]  [fim de linha]
+ * @param {{type: string}} element
+ * @param {{type: string}[]} inside
+ * @param {number} current
+ * @returns {boolean} isFunctionCallSequence
+ */
+const isFunctionCallSequence = (element, inside, current) => {
+  if (element.type !== "CodeCave") return false;
+  const prevElement = inside[current - 1];
+  const nextElement = inside[current + 1];
+
+  const sequence = agroupElemetsType(prevElement, element, nextElement);
+
+  return sequence === semanticSequences.functionCall;
+};
+
+/**
+ * Constroi objeto para chamada de função
+ * @param {{ type: "CodeCave"; arguments: {type: string; value: any}[]; callee: {type: 'Identifier', name: string} }} codeCave
+ * @returns {{type: "Call"; params: {type: string; value: any}[]; callee: string }}
+ */
+const functionCallBuilder = (codeCave) => ({
+  type: "Call",
+  params: codeCave.arguments,
+  callee: codeCave.callee.name,
+});
+
+/**
+ * Verifica se é uma estrutura de código                    [Word]    [CodeCave]    [CodeDomain]
+ * A estrutura de código precisa ter a seguinte sequencia   [if|for]  [argumentos]  [bloco de codigo]
+ * @param {{type: string}} element
+ * @param {{type: string}[]} inside
+ * @param {number} current
+ * @returns {boolean} isCodeStructureSequence
+ */
+const isCodeStructureSequence = (element, inside, current) => {
+  if (element.type !== "CodeDomain") return false;
+  const prevElement = inside[current - 1];
+  const doublePrevElement = inside[current - 2];
+
+  const sequence = agroupElemetsType(doublePrevElement, prevElement, element);
+
+  return sequence === semanticSequences.codeStructure;
+};
+
+/**
+ * Verifica se if é um else if
+ * @param {{ type: string; value: string }} codeCavePrevElement
+ * @returns {{type: "Call"; params: {type: string; value: any}[]; callee: string }}
+ */
+const identifyElseIf = (codeCavePrevElement) => {
+  if (!codeCavePrevElement) return false;
+  if (codeCavePrevElement.type !== "Word") return false;
+
+  const codeCavePrevElementValue = codeCavePrevElement.value.toLowerCase();
+
+  return codeCavePrevElementValue === "else";
+};
+
+/**
+ * Constroi objeto para chamada de if e elseif
+ * @param {{ type: "CodeCave"; arguments: {type: string; value: any}[]; callee: {type: 'Identifier', name: string} }} codeCave
+ * @param {any} processedBody
+ * @param {boolean} isElseIf
+ * @returns {{type: "if" | "elseif"; codition: any; body: any }}
+ */
+const ifCallBuilder = (codeCave, processedBody, isElseIf) => {
+  return {
+    type: isElseIf ? "elseif" : "if",
+    condition: codeCave.arguments,
+    body: processedBody,
+  };
+};
+
+/**
+ * Constroi objeto para chamada de while
+ * @param {{ type: "CodeCave"; arguments: {type: string; value: any}[]; callee: {type: 'Identifier', name: string} }} codeCave
+ * @param {any} processedBody
+ * @param {boolean} isElseIf
+ * @returns {{type: "while"; codition: any; body: any }}
+ */
+const whileCallBuilder = (codeCave, processedBody) => {
+  return {
+    type: "while",
+    condition: codeCave.arguments,
+    body: processedBody,
+  };
+};
+
+/**
+ * Constroi objeto para chamada de for
+ * @param {{ type: "CodeCave"; arguments: {type: string; value: any}[]; callee: {type: 'Identifier', name: string} }} codeCave
+ * @param {any} processedBody
+ * @param {boolean} isElseIf
+ * @returns {{type: "for"; codition: any; body: any }}
+ */
+const forCallBuilder = (codeCave, processedBody) => {
+  return {
+    type: "for",
+    condition: codeCave.arguments,
+    body: processedBody,
+  };
+};
+
+/**
+ * Constroi objeto para bloco de switch
+ * @param {{ type: "CodeCave"; arguments: {type: string; value: any}[]; callee: {type: 'Identifier', name: string} }} codeCave
+ * @param {any} processedBody
+ * @param {boolean} isElseIf
+ * @returns {{type: "switch"; codition: any; body: any }}
+ */
+const switchCallBuilder = (codeCave, cases) => {
+  return {
+    type: "switch",
+    condition: codeCave.arguments,
+    body: cases,
+  };
+};
+
+/**
+ * Verifica se é uma estrutura de código sem argumentos     [Word]     [CodeDomain]
+ * A estrutura de código precisa ter a seguinte sequencia   [else|do]  [bloco de codigo]
+ * @param {{type: string}} element
+ * @param {{type: string}[]} inside
+ * @param {number} current
+ * @returns {boolean} isSmallCodeStructureSequence
+ */
+const isSmallCodeStructureSequence = (element, inside, current) => {
+  if (element.type !== "CodeDomain") return false;
+  const prevElement = inside[current - 1];
+
+  const sequence = agroupElemetsType(prevElement, element);
+
+  return sequence === semanticSequences.smallCodeStructure;
+};
+
+/**
+ * Constroi objeto para chamada de else
+ * @param {any} processedBody
+ * @returns {{type: "else"; codition: any; body: any }}
+ */
+const elseCallBuilder = (processedBody) => {
+  return {
+    type: "else",
+    body: processedBody,
+  };
+};
+
+/**
+ * Verifica se é uma estrutura de uma struct                 [Word]    [CodeDomain]        [Word]    [CodeCave]
+ * A estrutura de código precisa ter a seguinte sequencia    [do]      [bloco de codigo]   [while]   [bloco de codigo]
+ * @param {{type: string}} element
+ * @param {{type: string}[]} inside
+ * @param {number} current
+ * @returns {boolean} isCodeStructureSequence
+ */
+const isDoCodeStructureSequence = (element, inside, current) => {
+  if (element.type !== "CodeDomain") return false;
+  const prevElement = inside[current - 1];
+  const nextElement = inside[current + 1];
+  const doubleNextElement = inside[current + 2];
+
+  const sequence = agroupElemetsType(
+    prevElement,
+    element,
+    nextElement,
+    doubleNextElement
+  );
+
+  return sequence === semanticSequences.doCodeStructure;
+};
+
+/**
+ * Constroi objeto para chamada de do
+ * @param {any} processedBody
+ * @returns {{type: "do"; codition: any; body: any }}
+ */
+const doCallBuilder = (codeCave, processedBody) => {
+  return {
+    type: "do",
+    condition: codeCave.arguments,
+    body: processedBody,
+  };
+};
+
+/**
+ * Verifica se é uma estrutura de código do                 [Word]      [Word]            [CodeDomain]
+ * A estrutura de código precisa ter a seguinte sequencia   [struct]    [nome da struct]  [bloco de codigo]
+ * @param {{type: string}} element
+ * @param {{type: string}[]} inside
+ * @param {number} current
+ * @returns {boolean} isStructCodeStructureSequence
+ */
+const isStructCodeStructureSequence = (element, inside, current) => {
+  if (element.type !== "CodeDomain") return false;
+  const prevElement = inside[current - 1];
+  const doublePrevElement = inside[current - 2];
+  const nextElement = inside[current + 1];
+
+  const sequence = agroupElemetsType(
+    doublePrevElement,
+    prevElement,
+    element,
+    nextElement
+  );
+
+  return sequence === semanticSequences.structCodeStructure;
+};
+
+/**
+ * Constroi objeto para chamada de struct
+ * @param {any} processedBody
+ * @returns {{type: "struct"; codition: any; body: any }}
+ */
+const structCallBuilder = (structName, processedBody) => {
+  return {
+    type: "struct",
+    name: structName,
+    body: processedBody,
+  };
+};
+
+/**
+ * Constroi objeto para um statement
+ * @param {any} processedBody
+ * @returns {{type: "Statement"; codition: any; body: any }}
+ */
+const statementBuilder = (value) => {
+  return {
+    type: "Statement",
+    value,
+  };
+};
